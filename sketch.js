7aeb058b6c7842d8079faf89a56b777478888dff
Ross Rosenlof <rrosenlo@byu.edu>
2020-08-26 14:57:43 -0600

    adding pixelate

--- sketch.js
+++ sketch.js
@@ -1,37 +1,30 @@
-// 2-D Perlin Noise for non-flat colors
+let img;
 
-var start = 0;
-var inc = 0.01;
+function preload() {
+  img = loadImage('assets/test.jpg');
+}
 
-function setup() {
-  createCanvas(200, 200);
-  pixelDensity(1);
+function setup(){
+  createCanvas(600,400);
+  smallPoint = 4;
+  largePoint = 20;
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  
+  img.loadPixels();
+  
+  img.filter(GRAY);
 }
 
 function draw() {
-  
-  var yoff = 0;
-
-  loadPixels();
-  for (var y =0; y < height; y++) {
-    var xoff = 0;
-    for (var x =0; x < width; x++) {
-      var index = (x + y * width) * 4;
-      var r = noise(xoff, yoff) * 8 * floor(random(-1,2));
-      // console.log(r);
-      pixels[index] = 20 + r;
-      r = noise(xoff, yoff) * 8 * floor(random(-1,2));
-      pixels[index+1] = 100 + r;
-      r = noise(xoff, yoff) * 8 * floor(random(-1,2));
-      pixels[index+2] = 150 + r;
-      r = noise(xoff, yoff) * 8 * floor(random(-1,2));
-      pixels[index+3] = 200;
-
-      xoff += inc;
-
+  for (let x = 0; x < img.width; x += 15) {
+    for (let y = 0; y < img.height; y += 15) {
+      let pix = img.get(x, y);
+      fill(pix);
+      rect(x, y, largePoint, largePoint);
     }
-    yoff += inc;
   }
-  updatePixels();
+
   noLoop();
 }
