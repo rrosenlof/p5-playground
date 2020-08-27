38a7e77cccd42f049f779e93e90899c10c7caf98
Ross Rosenlof <rrosenlo@byu.edu>
2020-08-27 09:43:57 -0600

    trying things out

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ let arrSum;
 let widthPerc;
 
 function preload() {
-  img = loadImage('assets/test.jpg');
+  img = loadImage('assets/test3.jpg');
 }
 
 function setup() {
   createCanvas(600, 600);
-  pixelLength = 5;
+  pixelLength = 4;
   pixelWidth = 3;
   imageMode(CENTER);
   noStroke();
@@ -21,42 +21,50 @@ function setup() {
 }
 
 function draw() {
+  // for (let x = 0; x < img.width; x += 5) {
+  //   for (let y = 0; y < img.height; y += 5) {
+  //     let pix = img.get(x, y);
+  //     arrSum = pix.reduce(getSum, 0);
+  //     widthPerc = (arrSum) / (1020);
+  //     if (arrSum > 600 && arrSum < 900) {
+  //       colorSquare = ('#e3a17d');
+  //       fill(colorSquare);
+  //       rect(x+1.25, y, (pixelWidth * widthPerc), pixelLength);
+  //     }
+  //   }
+  // }
   for (let x = 0; x < img.width; x += 5) {
     for (let y = 0; y < img.height; y += 5) {
       let pix = img.get(x, y);
       arrSum = pix.reduce(getSum, 0);
-      widthPerc = (arrSum) / (1020);
-      if (arrSum > 600 && arrSum < 900) {
-        colorSquare = ('yellow');
+      widthPerc = arrSum/1020;
+      // if (arrSum < 750 && arrSum > 400) {
+        colorSquare = ('brown');
         fill(colorSquare);
-        rect(x, y, pixelWidth * widthPerc, pixelLength);
-      }
+        rect(x+2.5, y, (pixelWidth * widthPerc), pixelLength);
+      // }
     }
   }
-  for (let x = 0; x < img.width; x += 5) {
+  for (let x = 0; x < img.width; x += 10) {
     for (let y = 0; y < img.height; y += 5) {
       let pix = img.get(x, y);
       arrSum = pix.reduce(getSum, 0);
-      widthPerc = arrSum/750;
-      if (arrSum < 750 && arrSum > 400) {
-        colorSquare = ('cyan');
+      widthPerc = 1 - (arrSum / 500);
+      // if (arrSum < 550) {
+        colorSquare = ('green');
         fill(colorSquare);
-        rect(x, y, pixelWidth * widthPerc, pixelLength);
-      }
+        rect(x, y, (pixelWidth * widthPerc), pixelLength);
+      // }
     }
   }
-  for (let x = 0; x < img.width; x += 5) {
-    for (let y = 0; y < img.height; y += 5) {
-      let pix = img.get(x, y);
-      arrSum = pix.reduce(getSum, 0);
-      widthPerc = (arrSum / 550);
-      if (arrSum < 550) {
-        colorSquare = ('magenta');
-        fill(colorSquare);
-        rect((x), y, pixelWidth * widthPerc, pixelLength);
-      }
-    }
-  }
+
+  // for (let x = 0; x < img.width; x += 15) {
+  //   for (let y = 0; y < img.height; y += 15) {
+  //     let pix = img.get(x, y);
+  //     fill(pix);
+  //     rect(x, y, 20, 20);
+  //   }
+  // }
 
   noLoop();
 }
