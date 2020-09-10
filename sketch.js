05e0cd056764621a1e02c5fb58c2c7f8ce0c3035
Ross Rosenlof <rrosenlo@byu.edu>
2020-09-10 15:43:54 -0600

    playing with line drawings, noise

--- sketch.js
+++ sketch.js
@@ -4,16 +4,16 @@ let arrSum;
 let widthPerc;
 
 function preload() {
-  img = loadImage('assets/test5.jpg');
+  img = loadImage('assets/test7.jpg');
 }
 
 function setup() {
   createCanvas(img.width, img.height);
   pixelLength = 3;
-  pixelWidth = 3;
+  pixelWidth = 1;
   imageMode(CENTER);
   noStroke();
-  background(124);
+  background(240);
 
   img.loadPixels();
 
@@ -21,23 +21,18 @@ function setup() {
 }
 
 function draw() {
-  for (let x = 0; x < img.width; x += 5) {
+  for (let x = 0; x < img.width; x += 8) {
     for (let y = 0; y < img.height; y += 5) {
       let pix = img.get(x, y);
       arrSum = pix.reduce(getSum, 0);
-      widthPerc = arrSum / 1020;
-      if (arrSum > 600 && arrSum < 900) {
-        colorSquare = ('purple');
+      widthPerc = 8 - Math.ceil((arrSum / 1020) * 10);
+      let xoff = 0.001
+      let yoff = 0.001
+      for (let i = 0; i < widthPerc; i += 1) {
+        let noise_adj = noise(xoff, yoff) * (i+1)
+        colorSquare = ('#234459');
         fill(colorSquare);
-        rect(x, y, pixelWidth * widthPerc, pixelLength);
-      } else if (arrSum < 750 && arrSum > 400) {
-        colorSquare = ('white');
-        fill(colorSquare);
-        rect(x, y, pixelWidth * widthPerc, pixelLength);
-      } else if (arrSum < 550) {
-        colorSquare = ('black');
-        fill(colorSquare);
-        rect(x, y, pixelWidth * widthPerc, pixelLength);
+        rect(x + (i * 2) * noise_adj, y - (i * 2)* -noise_adj, pixelWidth, pixelLength);
       }
     }
   }
